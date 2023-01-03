35aaa66636697070ccd01c055f9e7721f4afdbdc
thanaschai <thanaschai@gmail.com>
2023-01-03 15:46:03 +0700

    lab11_2done

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -3,13 +3,20 @@
 #include<string>
 using namespace std;
 
+	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 int main (){
 	ifstream source;
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string t ;
+	dest << "-------------------- BOOM ---------------------" << endl;  
+	while (getline(source,t)){
+		dest << t <<endl;
+	}
+	dest << "-------------------- HA!! ---------------------" ;
+	
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
