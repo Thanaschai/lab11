0445c12a33c61cc8609b3f5b605e9e41ae2ff940
thanaschai <thanaschai@gmail.com>
2023-01-03 16:14:27 +0700

    lab11_3.cpp

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream score ;
+    score.open("score.txt");
+    double sum;
+    double count = 0 ;
+    double mean ;
+    double xpowtwo ;
+    double sd ;
+    string t ;
+    while (getline(score,t)) {
+        sum += stod(t);
+        xpowtwo += pow(stod(t),2);
+        count++ ;
+    }
+    mean = sum/count ;
+    sd = sqrt((xpowtwo/count)-(pow(mean,2))) ;
+    cout << "Number of data = " << count << endl ;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd ;
+
+
+    return 0 ;
+}
